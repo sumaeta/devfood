45c10f01f1364fa28ce5c4691d7fc7e32fd0ab90
sumaeta <brenosumaeta33@gmail.com>
2022-10-01 17:57:04 -0300

    init

---
+++ src/main/java/com/code/devfood/DevfoodApplication.java
@@ -0,0 +1,13 @@
+package com.code.devfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/devfood/DevfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.devfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
