dc461f403a11bafb3224ef0497487eb3698dc9fc
sumaeta <brenosumaeta33@gmail.com>
2022-10-07 19:09:57 -0300

    Propriedade Nullable no Restaurante

--- src/main/java/com/code/devfood/domain/model/Restaurante.java
+++ src/main/java/com/code/devfood/domain/model/Restaurante.java
@@ -2,10 +2,12 @@ package com.code.devfood.domain.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -15,10 +17,14 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false)
 	private String nome;
+	
+	@Column(nullable = false)
 	private BigDecimal taxaFrete;
 	
 	@ManyToOne
+	@JoinColumn(nullable = false)
 	private Cozinha cozinha;
 	
 	public Long getId() {
