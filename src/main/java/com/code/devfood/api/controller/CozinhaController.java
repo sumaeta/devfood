b3dc9f188ea9cdc8f922b29e2d71066b90e07dd2
sumaeta <brenosumaeta33@gmail.com>
2022-10-10 11:11:31 -0300

    Adicionado o PUT na CozinhaController

--- src/main/java/com/code/devfood/api/controller/CozinhaController.java
+++ src/main/java/com/code/devfood/api/controller/CozinhaController.java
@@ -1,6 +1,5 @@
 package com.code.devfood.api.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,40 +9,55 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.code.devfood.domain.model.Cozinha;
 import com.code.devfood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping(value = "/cozinhas", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@RequestMapping(value = "/cozinhas", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class CozinhaController {
-	
+
 	private final CozinhaRepository repository;
-	
+
 	@Autowired
 	public CozinhaController(CozinhaRepository repository) {
 		this.repository = repository;
 	}
 
 	@GetMapping
-	public ResponseEntity<List<Cozinha>> listar(){
+	public ResponseEntity<List<Cozinha>> listar() {
 		List<Cozinha> cozinhas = this.repository.findAll();
 		return ResponseEntity.ok(cozinhas);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Cozinha> buscar(@PathVariable Long id){
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 		Cozinha cozinha = this.repository.findById(id).get();
 		return ResponseEntity.ok(cozinha);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Cozinha> salvar(@RequestBody Cozinha cozinha){
+	public ResponseEntity<Cozinha> salvar(@RequestBody Cozinha cozinha) {
 		this.repository.save(cozinha);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+		Cozinha obj = this.buscar(id).getBody();
+
+		if (obj != null) {
+			obj.setNome(cozinha.getNome());
+
+			this.salvar(obj);
+			return ResponseEntity.ok().build();
+
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
