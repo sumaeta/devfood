8524b096659c6e475098688ee0a1e8e2ca5d4df3
sumaeta <brenosumaeta33@gmail.com>
2022-10-10 10:50:29 -0300

    Inserindo o metodo de salvar cozinha

--- src/main/java/com/code/devfood/api/controller/CozinhaController.java
+++ src/main/java/com/code/devfood/api/controller/CozinhaController.java
@@ -1,14 +1,19 @@
 package com.code.devfood.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.code.devfood.domain.model.Cozinha;
 import com.code.devfood.domain.repository.CozinhaRepository;
@@ -34,5 +39,11 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id){
 		Cozinha cozinha = this.repository.findById(id).get();
 		return ResponseEntity.ok(cozinha);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cozinha> salvar(@RequestBody Cozinha cozinha){
+		this.repository.save(cozinha);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 }
