65aebf0783a0c3b9ef9a9ca644357a8d64cce4f2
sumaeta <brenosumaeta33@gmail.com>
2022-10-10 14:42:32 -0300

    Implementando metodo post no restaurante

--- src/main/java/com/code/devfood/api/controller/RestauranteController.java
+++ src/main/java/com/code/devfood/api/controller/RestauranteController.java
@@ -3,9 +3,12 @@ package com.code.devfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,21 +20,27 @@ import com.code.devfood.domain.service.RestauranteService;
 public class RestauranteController {
 
 	private final RestauranteService service;
-	
+
 	@Autowired
 	public RestauranteController(RestauranteService service) {
 		this.service = service;
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<Restaurante>> listar(){
+	public ResponseEntity<List<Restaurante>> listar() {
 		List<Restaurante> restaurantes = this.service.listar();
 		return ResponseEntity.ok(restaurantes);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Restaurante> buscar(@PathVariable Long id){
+	public ResponseEntity<Restaurante> buscar(@PathVariable Long id) {
 		Restaurante restaurantes = this.service.buscar(id);
 		return ResponseEntity.ok(restaurantes);
+	}
+
+	@PostMapping
+	public ResponseEntity<Restaurante> salvar(@RequestBody Restaurante restaurante) {
+		this.service.salvar(restaurante);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 }
--- src/main/java/com/code/devfood/domain/service/RestauranteService.java
+++ src/main/java/com/code/devfood/domain/service/RestauranteService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.code.devfood.domain.exception.EntidadeNaoEncontradaException;
+import com.code.devfood.domain.model.Cozinha;
 import com.code.devfood.domain.model.Restaurante;
 import com.code.devfood.domain.repository.RestauranteRepository;
 
@@ -15,10 +16,12 @@ import com.code.devfood.domain.repository.RestauranteRepository;
 public class RestauranteService {
 
 	private final RestauranteRepository repository;
+	private final CozinhaService service;
 	
 	@Autowired
-	public RestauranteService(RestauranteRepository repository) {
+	public RestauranteService(RestauranteRepository repository, CozinhaService service) {
 		this.repository = repository;
+		this.service = service;
 	}
 	
 	@Transactional
@@ -30,5 +33,18 @@ public class RestauranteService {
 	public Restaurante buscar(Long id) {
 		Optional<Restaurante> obj = this.repository.findById(id);
 		return obj.orElseThrow(()-> new EntidadeNaoEncontradaException(String.format("Restaurante com o código %d não foi encontrado", id)));
+	}
+
+	@Transactional
+	public void salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = this.service.buscar(cozinhaId);
+		
+		if (cozinha == null ) {
+			throw new EntidadeNaoEncontradaException(String.format("Não existe o cadastro de cozinha com o código: %d", cozinhaId));
+		}
+		
+		restaurante.setCozinha(cozinha);
+		this.repository.save(restaurante);
 	}
 }
