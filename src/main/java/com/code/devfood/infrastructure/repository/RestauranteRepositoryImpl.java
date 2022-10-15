d4c178ab9b3a2974d0e9c2e5cf9f9a7d468b1c2c
sumaeta <brenosumaeta33@gmail.com>
2022-10-15 14:21:53 -0300

    Implementando consulta com Criteria API E Predicate

--- src/main/java/com/code/devfood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/code/devfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,6 +9,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -57,7 +59,17 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class);
+		
+		Root<Restaurante> root =  criteria.from(Restaurante.class);
+		Predicate predicateNome = builder.like(root.get("nome"), "%" + nome + "%");
+		
+		Predicate predicateTaxaInicial = builder
+				.greaterThanOrEqualTo(root.get("taxaFrete"), taxaInicial);
+		
+		Predicate predicateTaxaFinal = builder
+				.lessThanOrEqualTo(root.get("taxaFrete"), taxaFinal);
+		
+		criteria.where(predicateNome, predicateTaxaInicial, predicateTaxaFinal);
 		
 		return manager.createQuery(criteria).getResultList();
 	}
