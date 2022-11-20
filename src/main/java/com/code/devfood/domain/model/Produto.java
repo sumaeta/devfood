913afd6036ddf93e01be34b66b5bb274543ece8f
sumaeta <brenosumaeta33@gmail.com>
2022-11-20 13:16:43 -0300

    mapeando relacionamento um-para-muitos

---
+++ src/main/java/com/code/devfood/domain/model/Produto.java
@@ -0,0 +1,80 @@
+package com.code.devfood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	private boolean ativo;
+	
+	@ManyToOne
+	private Restaurante restaurante;
+
+	public Produto() {
+	}
+
+	public Produto(Long id, String nome, BigDecimal preco, boolean ativo, Restaurante restaurante) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.ativo = ativo;
+		this.restaurante = restaurante;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Restaurante getRestaurante() {
+		return restaurante;
+	}
+
+	public void setRestaurante(Restaurante restaurante) {
+		this.restaurante = restaurante;
+	}
+}
