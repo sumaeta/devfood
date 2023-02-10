56ff980298e1b79733eb3e21d0493ba283b695f7
sumaeta <brenosumaeta33@gmail.com>
2023-02-10 00:48:22 -0300

    Lançando exceções customizadas anotadas com @ResponseStatus

--- src/main/java/com/code/devfood/api/controller/CidadeController.java
+++ src/main/java/com/code/devfood/api/controller/CidadeController.java
@@ -4,14 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.code.devfood.domain.model.Cidade;
 import com.code.devfood.domain.service.CidadeService;
@@ -45,9 +38,9 @@ public class CidadeController {
 	}
 
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Cidade> remover(@PathVariable Long id) {
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
 		this.service.remover(id);
-		return ResponseEntity.noContent().build();
 	}
 
 	@PutMapping(value = "/{id}")
