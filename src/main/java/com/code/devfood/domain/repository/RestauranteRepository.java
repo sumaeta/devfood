9046cd2b53084eb472d8de9d881b6532c9dea48d
sumaeta <brenosumaeta33@gmail.com>
2023-02-26 13:56:50 -0300

    Ajuste na versão do maven

--- src/main/java/com/code/devfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/code/devfood/domain/repository/RestauranteRepository.java
@@ -3,7 +3,6 @@ package com.code.devfood.domain.repository;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
